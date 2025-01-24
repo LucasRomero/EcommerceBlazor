9025796cd42e5f452b3e3a547e2915566d558b36
LucasRomero <romerolucasdaniel@outlook.com>
2025-01-24 17:32:24 -0300

    changes

---
+++ Ecommerce.API/Program.cs
@@ -0,0 +1,46 @@
+using Ecommerce.Repositorio.Contrato;
+using Ecommerce.Repositorio.DBContext;
+using Ecommerce.Repositorio.Implementacion;
+using Microsoft.EntityFrameworkCore;
+using Ecommerce.Utilidades;
+using Ecommerce.Servicio.Contrato;
+using Ecommerce.Servicio.Implementacion;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<DbecommerceContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSQL"))
+);
+
+builder.Services.AddTransient(typeof(IGenericoRepositorio<>), typeof(GenericoRepositorio<>));
+builder.Services.AddScoped<IVentaRepositorio, VentaRepositorio>();
+
+builder.Services.AddScoped<ICategoriaServicio, CategoriaServicio>();
+builder.Services.AddScoped<IDashboardServicio, DashboardServicio>();
+builder.Services.AddScoped<IProductoServicio, ProductoServicio>();
+builder.Services.AddScoped<IUsuarioServicio, UsuarioServicio>();
+builder.Services.AddScoped<IVentaServicio, VentaServicio>();
+builder.Services.AddScoped<IUsuarioServicio, UsuarioServicio>();
+
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("NuevaPolitica", app =>
+    {
+        app.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("NuevaPolitica");
+
+// Configure the HTTP request pipeline.
+
+app.Run();
+
